346aecbf9baa29ab84a0a8fc6836a63b59286821
Ivan Prozorskov <ivan.proza@gmail.com>
2022-02-08 16:10:15 +0300

    PAY-186 created new pages

---
+++ src/pages/city24/terminals/:create.js
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { Box, Container, Card, CardHeader, Divider } from "@material-ui/core";
+import useSettings from "@hooks/useSettings";
+import axios from "@lib/axios";
+import { app } from "@root/config";
+import { useTranslation } from "react-i18next";
+import CreateTerminalForm from "./includes/CreateTerminalForm";
+import toast from "react-hot-toast";
+import { BackButton } from "@comp/core/buttons";
+
+const UserIdUpdate = () => {
+  const { settings } = useSettings();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  const handleSubmit = async (values) => {
+    try {
+      await axios
+        .post(`${app.api}/terminals/city`, { ...values })
+        .then((response) => {
+          toast.success(t("Success update"));
+          navigate(`/city24/terminals`);
+        });
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>{t("Terminals Model Create")}</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: "background.default",
+          minHeight: "100%",
+          py: 2,
+        }}
+      >
+        <Container maxWidth={settings.compact ? "xl" : false}>
+          <BackButton action={() => navigate(`/city24/terminals`)} />
+          <Box sx={{ minWidth: 700 }}>
+            <Card sx={{ mt: 2 }}>
+              <CardHeader title={t("Terminals Model Create")} />
+              <Divider />
+              <CreateTerminalForm callback={handleSubmit} />
+            </Card>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default UserIdUpdate;
