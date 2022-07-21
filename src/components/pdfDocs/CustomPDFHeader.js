54b24907e657bcbbe32a5696e6f4237b283c244d
dmitry.kalinin <vega-kim@mail.ru>
2022-07-21 16:45:49 +0300

    PAY-191, add pdf transaction doc

---
+++ src/components/pdfDocs/CustomPDFHeader.js
@@ -0,0 +1,37 @@
+import { Text, View, StyleSheet } from '@react-pdf/renderer';
+
+const { head, alignEnd, alignCenter, hugeMarginBottom } = StyleSheet.create({
+  head: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    fontSize: '11px',
+  },
+  alignEnd: {
+    alignSelf: 'flex-end',
+  },
+  alignCenter: {
+    alignSelf: 'center',
+  },
+  hugeMarginBottom: {
+    marginBottom: '50px',
+  },
+});
+
+const CustomPDFHeader = ({ contractNumber, contractDate, businessName }) => {
+  return (
+    <View style={head}>
+      <Text>{`Вих. № ${contractNumber}`}</Text>
+      <Text>Від {contractDate} </Text>
+      <Text style={[alignEnd, hugeMarginBottom]}>ТОВ «{businessName}» </Text>
+      <Text style={[alignCenter, { marginBottom: '15px' }]}>
+        Шановні партнери!
+      </Text>
+      <Text style={[alignCenter, { marginBottom: '15px' }]}>
+        Цим листом виражаємо Вам нашу повагу та повідомляємо наступне:
+      </Text>
+    </View>
+  );
+};
+
+export default CustomPDFHeader;
