c95f11a7d8e3ba53e179e18f964431497737273a
Vladislav <vladfyklev@gmail.com>
2024-11-15 18:05:28 +0200

    fix url

--- src/pages/transitAccount/helper.js
+++ src/pages/transitAccount/helper.js
@@ -1,7 +1,6 @@
-import { app } from '@root/config';
 import axios from 'axios';
 
-const baseURL = `${app.apiNewService}`;
+const baseURL = `https://bcc3.paytech.agis.work/api`;
 
 let token = ''; // Здесь будем хранить токен после авторизации
 
@@ -10,7 +9,7 @@ export const auth = async (data) => {
   const response = await axios.post(`${baseURL}/auth`, data, {
     headers: {
       // Убираем токен
-      Authorization: '',
+      Authorization: ''
     }
   });
   token = response.data.token; // Сохраняем токен
@@ -20,20 +19,23 @@ export const auth = async (data) => {
 // Функция для добавления Authorization в заголовки всех запросов
 const getAuthHeaders = () => {
   return {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${token}`
   };
 };
 
 // 2. Получение списка всех компаний
 export const getAllCompanies = async (page, count) => {
-  const response = await axios.get(`${baseURL}/company?page=${page}&count=${count}`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
-  });
+  const response = await axios.get(
+    `${baseURL}/company?page=${page}&count=${count}`,
+    {
+      headers: getAuthHeaders() // Используем токен в заголовках
+    }
+  );
   return response.data;
 };
 export const getAllCompaniesTransactions = async () => {
   const response = await axios.get(`${baseURL}/company`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
@@ -41,7 +43,7 @@ export const getAllCompaniesTransactions = async () => {
 // 3. Создание новой компании
 export const createCompany = async (data) => {
   const response = await axios.post(`${baseURL}/company`, data, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
@@ -49,7 +51,7 @@ export const createCompany = async (data) => {
 // 4. Получение инфы об определенной компании
 export const getCompanyId = async (companyId) => {
   const response = await axios.get(`${baseURL}/company/${companyId}`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
@@ -57,7 +59,7 @@ export const getCompanyId = async (companyId) => {
 // 5. Изменение информации об определенной компании
 export const updateCompany = async (companyId, data) => {
   const response = await axios.put(`${baseURL}/company/${companyId}`, data, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
@@ -65,7 +67,7 @@ export const updateCompany = async (companyId, data) => {
 // 6. Удаление выбранной компании
 export const deleteCompany = async (companyId) => {
   const response = await axios.delete(`${baseURL}/company/${companyId}`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
@@ -73,39 +75,49 @@ export const deleteCompany = async (companyId) => {
 // 7. Получение всех транзитных счетов
 export const getAllAccounts = async () => {
   const response = await axios.get(`${baseURL}/account`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
 
 // 8. Получение списка всех транзакций
 export const getAllTransactions = async (page, count) => {
-  const response = await axios.get(`${baseURL}/transaction?page=${page}&count=${count}`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
-  });
+  const response = await axios.get(
+    `${baseURL}/transaction?page=${page}&count=${count}`,
+    {
+      headers: getAuthHeaders() // Используем токен в заголовках
+    }
+  );
   return response.data;
 };
 
 // 9. Создание транзакции
 export const createTransaction = async (data) => {
   const response = await axios.post(`${baseURL}/transaction`, data, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
+    headers: getAuthHeaders() // Используем токен в заголовках
   });
   return response.data;
 };
 
 // 10. Отправка верифицированной с нашей стороны транзакции в банк
 export const acceptTransaction = async (transactionId) => {
-  const response = await axios.post(`${baseURL}/transaction/${transactionId}/accept`, {}, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
-  });
+  const response = await axios.post(
+    `${baseURL}/transaction/${transactionId}/accept`,
+    {},
+    {
+      headers: getAuthHeaders() // Используем токен в заголовках
+    }
+  );
   return response.data;
 };
 
 // 11. Обновление статуса транзакции со стороны банка
 export const getTransactionStatus = async (transactionId) => {
-  const response = await axios.get(`${baseURL}/transaction/${transactionId}/status`, {
-    headers: getAuthHeaders(), // Используем токен в заголовках
-  });
+  const response = await axios.get(
+    `${baseURL}/transaction/${transactionId}/status`,
+    {
+      headers: getAuthHeaders() // Используем токен в заголовках
+    }
+  );
   return response.data;
 };
