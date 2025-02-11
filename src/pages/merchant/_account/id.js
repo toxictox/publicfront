659cd43eb969bf3d5f5356fbb4e98986196725a5
Vladislav <vladfyklev@gmail.com>
2025-02-11 15:23:18 +0200

    KZP-743/add statement in merchant

--- src/pages/merchant/_account/id.js
+++ src/pages/merchant/_account/id.js
@@ -1,27 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { BackButton, GroupTable } from '@comp/core/buttons';
+import { TableStatic } from '@comp/core/tables/index';
+import OperationsList from '@comp/merchant/account/OperationsList';
+import useAuth from '@hooks/useAuth';
+import useMounted from '@hooks/useMounted';
+import useSettings from '@hooks/useSettings';
+import axios from '@lib/axios';
 import {
   Box,
-  Container,
   Card,
-  TableRow,
-  TableCell,
   CardHeader,
+  Container,
   Divider,
-} from "@material-ui/core";
-import useMounted from "@hooks/useMounted";
-import useSettings from "@hooks/useSettings";
-import axios from "@lib/axios";
-import { app } from "@root/config";
-import { useTranslation } from "react-i18next";
-import { TableStatic } from "@comp/core/tables/index";
-import { GroupTable, BackButton } from "@comp/core/buttons";
-import { useDispatch } from "react-redux";
-import useAuth from "@hooks/useAuth";
-import { toLocaleDateTime } from "@lib/date";
-import OperationsList from "@comp/merchant/account/OperationsList";
-
+  TableCell,
+  TableRow
+} from '@material-ui/core';
+import { app } from '@root/config';
+import { useCallback, useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import StatementMerchant from './StatementMerchant';
 
 const AccountId = () => {
   const mounted = useMounted();
@@ -30,26 +29,28 @@ const AccountId = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const { user, getAccess } = useAuth();
-  const [ account, setAccount ] = useState({
-  });
+  const [account, setAccount] = useState({});
   const [dataList, setListData] = useState({
-    data: [],
+    data: []
   });
   const dispatch = useDispatch();
 
   const getItem = useCallback(async () => {
     const response = await axios
-    .get(`${app.api}/merchant/${merchantId}/account/${accountId}`)
-    .then((response) => setAccount(response.data))
-    .catch((err) => {
+      .get(`${app.api}/merchant/${merchantId}/account/${accountId}`)
+      .then((response) => setAccount(response.data))
+      .catch((err) => {
         if (err.response.status === 404) {
-            navigate("*");
+          navigate('*');
         }
-    });
+      });
   }, [mounted, merchantId, accountId]);
 
   const formatAmount = (number) => {
-    return new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'KZT' }).format(number);
+    return new Intl.NumberFormat('ru-RU', {
+      style: 'currency',
+      currency: 'KZT'
+    }).format(number);
   };
 
   useEffect(() => {
@@ -59,26 +60,29 @@ const AccountId = () => {
   return (
     <>
       <Helmet>
-        <title>{t("Merchant Account")}</title>
+        <title>{t('Merchant Account')}</title>
       </Helmet>
       <Box
         sx={{
-          backgroundColor: "background.default",
-          minHeight: "100%",
-          py: 2,
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 2
         }}
       >
-        <Container maxWidth={settings.compact ? "xl" : false}>
-          <BackButton action={() => navigate(`/merchants/${merchantId}/account/`)} />
+        <Container maxWidth={settings.compact ? 'xl' : false}>
+          <BackButton
+            action={() => navigate(`/merchants/${merchantId}/account/`)}
+          />
           <Box sx={{ minWidth: 700 }}>
             <Card sx={{ mt: 2 }}>
               <CardHeader
-                title={t("Merchant Account Item")}
+                title={t('Merchant Account Item')}
                 action={
                   <GroupTable
                     actionUpdate={{
-                      access: getAccess("merchants", "update"),
-                      callback: () => navigate(`/merchants/id/${merchantId}/update`),
+                      access: getAccess('merchants', 'update'),
+                      callback: () =>
+                        navigate(`/merchants/id/${merchantId}/update`)
                     }}
                   />
                 }
@@ -86,29 +90,30 @@ const AccountId = () => {
               <Divider />
               <TableStatic>
                 <TableRow>
-                    <TableCell>{t("Id")}</TableCell>
-                    <TableCell>{account.id}</TableCell>
+                  <TableCell>{t('Id')}</TableCell>
+                  <TableCell>{account.id}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>{t("Name")}</TableCell>
+                  <TableCell>{t('Name')}</TableCell>
                   <TableCell>{account.name}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>{t("Balance")}</TableCell>
+                  <TableCell>{t('Balance')}</TableCell>
                   <TableCell>{formatAmount(account.balance / 100)}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>{t("Overdarft Limit")}</TableCell>
-                  <TableCell>{formatAmount(account.overdraftLimit / 100)}</TableCell>
+                  <TableCell>{t('Overdarft Limit')}</TableCell>
+                  <TableCell>
+                    {formatAmount(account.overdraftLimit / 100)}
+                  </TableCell>
                 </TableRow>
               </TableStatic>
             </Card>
           </Box>
         </Container>
-        <OperationsList
-            accountId={accountId}
-            merchantId={merchantId}
-        />
+        <OperationsList accountId={accountId} merchantId={merchantId} />
+
+        <StatementMerchant accountId={accountId} merchantId={merchantId} />
       </Box>
     </>
   );
